104f6bb62aa5d9c9c3aaf64f97da9be00948ad45
syshpagamo-hue <307474108+syshpagamo-hue@users.noreply.github.com>
2026-09-04 21:24:12 +0800

    Fix pharmacy basemap tile source

--- src/PharmacyMap.tsx
+++ src/PharmacyMap.tsx
@@ -92,8 +92,8 @@ export function PharmacyMap({
         }}
       >
         <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          url="https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+          url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <Marker position={[center.lat, center.lon]} icon={userIcon}>
           <Popup>{centerLabel}</Popup>
